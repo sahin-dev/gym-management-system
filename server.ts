28ab6902e0ae0357057a7349588587dd11cd82bb
sahin-dev <sahinsiraj360@gmail.com>
2025-04-12 00:08:03 +0600

    refoctoring the project

--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
-import app from "../src/app";
+import app from "./src/app";
 import { createServer } from "http";
 import dotenv from 'dotenv';
 
 
 
 dotenv.config()
-
+console.log("Hi")
 const PORT = process.env.PORT || 8000
 
 const server = createServer(app)
@@ -13,3 +13,5 @@ const server = createServer(app)
 server.listen(PORT, ()=>{
     console.log(`Server is listening on port: ${PORT}`)
 })
+
+export default server
